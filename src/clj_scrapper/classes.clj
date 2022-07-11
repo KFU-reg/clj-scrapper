b4544f2923e98d168316410a28bf8fa570c5d620
kfulain <89740818+stadial@users.noreply.github.com>
2022-07-11 20:46:23 +0300

    feat: cleaner API for `generate-url`
    Instead of using semster number as an
    arguments, one can use `:sem1`, `:sem2`,
    or `:summer`.

--- src/clj_scrapper/classes.clj
+++ src/clj_scrapper/classes.clj
@@ -10,19 +10,19 @@
   ALL Arguments are Strings!
 
   `current-year`: in Gregorian, e.g. 1443
-  `current-semster`: 10 = Sem1, 20 = Sem2, 30 = Summer.
+  `current-semster`: anything from `[:sem1, :sem2, :summer]`
                    THIS ONLY APPLIES TO 2 SEMSTERS PER YEAR!
-  `code`: Arbitrary code for each colege.
   `sex`: `:female` or `:male`
+  `code`: Arbitrary code for each colege.
 
   Example Return:
    'https://banner.kfu.edu.sa:7710/KFU/ws?p_trm_code=144310&p_col_code=22&p_sex_code=11'"
-  [current-year current-semster code sex]
+  [current-year current-semster sex code]
   (str base-url
        "p_trm_code="
        current-year
-       current-semster
+       (current-semster {:sem1 10, :sem2 20, :summer 30})
        "&p_col_code=" code
        "&p_sex_code=" (sex {:male 11, :female 12})))
 
-(generate-url 1443 10 22 :male)
+(generate-url 1443 :sem1 :male 22)
