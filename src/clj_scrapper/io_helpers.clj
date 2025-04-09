b9c0b324e818a214ad120c50ea3ed9a7ae5a55a4
kfulain <89740818+stadial@users.noreply.github.com>
2025-04-09 17:43:06 +0300

    feat: add http retries to make things more robust

--- src/clj_scrapper/io_helpers.clj
+++ src/clj_scrapper/io_helpers.clj
@@ -1,10 +1,22 @@
 (ns clj-scrapper.io-helpers
   (:require [net.cgrand.enlive-html :as html]
             [clojure.string :as s]
+            [clj-http.client :as http]
             [clojure.data.json :as json]
             [clojure.java.io :as io]))
 
-
+; This was added because github runners sometimes fails!!
+; Syntax error (SocketException) compiling at (...)
+(defn fetch-with-retries
+  [url]
+  (http/get url
+            {:socket-timeout 10000, ; 10 seconds
+             :conn-timeout 10000,
+             :retry-handler (fn [ex try-count http-context]
+                              (println "Retrying due to:" (class ex))
+                              (if (< try-count 3)
+                                (do (Thread/sleep (* 1000 try-count)) true)
+                                false))}))
 
 
 (defn caching-slurp
@@ -13,14 +25,15 @@
   [url]
   (let [sanitized_url (s/replace url #"(https?://|/)" "_")
         tmp_path (str "./output/cache/" sanitized_url)]
-    (try (slurp tmp_path); try slurping the cached file
+    (try (slurp tmp_path) ; try slurping the cached file
          (catch java.io.FileNotFoundException _
            ; if not cached, cache it...
-           (do (io/make-parents tmp_path); mkdir -p
-               (spit tmp_path (slurp url) :encoding "UTF-8");
+           (do (io/make-parents tmp_path) ; mkdir -p
+               (spit tmp_path (fetch-with-retries url) :encoding "UTF-8") ;
                (println "Wasn't cached:(")
                ; ...and re-slurp it.
                (slurp tmp_path))))))
+
 
 
 (defn get-dom
